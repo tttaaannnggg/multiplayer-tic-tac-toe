a42fa4b10d2c5bb00b5ea399b4307bcec48d6579
Sterling Deng <sterlingdeng@gmail.com>
2019-02-23 12:27:27 -0800

    readme in client

--- server/server.js
+++ server/server.js
@@ -3,6 +3,6 @@ const express = require("express");
 const server = express();
 const PORT = 3000;
 
-app.listen(3000, () => {
-  `Server listening on port ${port}`;
+server.listen(3000, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
